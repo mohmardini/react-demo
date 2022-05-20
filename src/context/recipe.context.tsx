b59ddc7674909a1507f99fbd990e54b5ce9bd690
mmardini <m.mardini@sirenanalytics.com>
2022-05-20 17:41:44 +0300

    fix

--- src/context/recipe.context.tsx
+++ src/context/recipe.context.tsx
@@ -12,19 +12,20 @@ interface ReactProviderPropTypes {
 
 export const RecipeContext = React.createContext({});
 
+const recipeReducer = (
+  state: RecipeState,
+  action: RecipeAction
+): RecipeState => {
+  switch (action.type) {
+    case RecipeActionType.SET_RECIPIES: {
+      return { ...state, recipeList: action.payload };
+    }
+  }
+};
+
 export const RecipeProvider = ({ children }: ReactProviderPropTypes) => {
   const [state, dispatch] = React.useReducer(recipeReducer, recipeInitialState);
 
-  const recipeReducer = (
-    state: RecipeState,
-    action: RecipeAction
-  ): RecipeState => {
-    switch (action.type) {
-      case RecipeActionType.SET_RECIPIES: {
-        return { ...state, recipeList: action.payload };
-      }
-    }
-  };
   return (
     <RecipeContext.Provider value={{ state, dispatch }}>
       {children}
