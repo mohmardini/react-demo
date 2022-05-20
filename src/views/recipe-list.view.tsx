217a8ce65e14da7d78de13862b7dfacdf2e8d2ff
mmardini <m.mardini@sirenanalytics.com>
2022-05-20 18:44:30 +0300

    context complete

--- src/views/recipe-list.view.tsx
+++ src/views/recipe-list.view.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { getAllRecipes } from 'src/api/recipe.api';
-import { RecipeActionType } from 'src/context/recipe.context';
+import { useRecipeContext } from 'src/hooks/recipe-context.hook';
+import { RecipeActionType } from 'src/models/recipe.model';
 
 const RecipeList = () => {
+  const { state, dispatch } = useRecipeContext();
+
   useEffect(() => {
-    const recipies = getAllRecipes()
-    recipies.then(recipies => dipatch({type: RecipeActionType.SET_RECIPIES}))
-  },[])
+    const recipies = getAllRecipes();
+    recipies.then((recipies) =>
+      dispatch({ type: RecipeActionType.SET_RECIPIES, payload: recipies })
+    );
+  }, [dispatch]);
   return (
     <div>
-      <Link to={'/edit/555665'}>RecipeList</Link>
+      <Link to={'/edit/555665'}></Link>
     </div>
   );
 };
