0580092f8e00fc457eb87349489c4823f40598c3
mmardini <m.mardini@sirenanalytics.com>
2022-05-20 14:47:07 +0300

    a

---
+++ src/views/recipe-add-edit.view.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecipeAddEdit = () => {
+  return <div>RecipeAddEdit</div>;
+};
+
+export default RecipeAddEdit;
--- src/views/recipe-edit.view.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const RecipeEdit = () => {
-  return <div>RecipeEdit</div>;
-};
-
-export default RecipeEdit;
