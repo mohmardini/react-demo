8d17e8fe7327dbe091f0a1efa3eabf5d66ea3e18
mmardini <m.mardini@sirenanalytics.com>
2022-05-20 14:54:23 +0300

    b

--- src/api/recipe.api.tsx
+++ src/api/recipe.api.tsx
@@ -1,13 +1,13 @@
-import { Recipe, RecipeState } from 'src/models/recipe.model';
+// import { Recipe, RecipeState } from 'src/models/recipe.model';
 
-export const getAllRecipes = async (): Promise<RecipeState['recipeList']> => {
-  return [];
-};
+// export const getAllRecipes = async (): Promise<RecipeState['recipeList']> => {
+//   return [];
+// };
 
-export const async getRecipe = (_id: Recipe['_id']): Recipe => {};
+// export const async getRecipe = (_id: Recipe['_id']): Recipe => {};
 
-export const async deleteRecipe = (_id: Recipe['_id']) => {};
+// export const async deleteRecipe = (_id: Recipe['_id']) => {};
 
-export const async addRecipe = (recipe: Recipe): boolean => {
-  return true;
-};
+// export const async addRecipe = (recipe: Recipe): boolean => {
+//   return true;
+// };
