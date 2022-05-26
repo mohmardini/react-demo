3b133124caa03cf2c1aa0e630906900796b5125b
mmardini <m.mardini@sirenanalytics.com>
2022-05-26 17:01:14 +0300

    a

--- src/layout/BasicLayout.tsx
+++ src/layout/BasicLayout.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { Container, Image ,Dropdown} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import Footer from './Footer';
-import { supportedLngs } from 'src/i18n';
+import { Container, Dropdown, Image } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { supportedLngs } from 'src/i18n';
+import Footer from './Footer';
 
 type BasicLayoutPropTypes = {
   children: React.ReactNode;
 };
 
-
-  
 const BasicLayout = (props: BasicLayoutPropTypes) => {
-
   const { i18n } = useTranslation();
   const changeLanguage = (language: string) => {
     i18n.changeLanguage(language);
@@ -24,37 +21,35 @@ const BasicLayout = (props: BasicLayoutPropTypes) => {
       <nav className="navbar navbar-light bg-light">
         <span className="navbar-brand mb-0 mx-auto ">
           <Link to="/">
-          <Image
-            src="/icons/logo.png"
-            alt="bg image"
-            fluid
-            style={{ width: 110 }}
-          />
-           </Link>
+            <Image
+              src="/icons/logo.png"
+              alt="bg image"
+              fluid
+              style={{ width: 110 }}
+            />
+          </Link>
         </span>
-       
 
         <Link to="/login" className="me-4 text-reset">
-              Login
+          Login
         </Link>
 
         <Dropdown>
-        <Dropdown.Toggle
-          variant="outline"
-          className="btn btn-outline-warning mb-1"
-        >
-          {i18n.language}
-        </Dropdown.Toggle>
+          <Dropdown.Toggle
+            variant="outline"
+            className="btn btn-outline-warning mb-1 mx-2"
+          >
+            {i18n.language}
+          </Dropdown.Toggle>
 
-        <Dropdown.Menu>
-          {supportedLngs.map((lang, idx) => (
-            <Dropdown.Item key={idx} onClick={() => changeLanguage(lang)}>
-              {lang}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-      
+          <Dropdown.Menu>
+            {supportedLngs.map((lang, idx) => (
+              <Dropdown.Item key={idx} onClick={() => changeLanguage(lang)}>
+                {lang}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
       </nav>
 
       {/* Bg Image */}
